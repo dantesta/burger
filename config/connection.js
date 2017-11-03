d355c9173dc24deeb3357dfb884a33ed5cab4420
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-11-03 17:22:37 -0400

    connectionjs updated

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "",
-        database: "burgers_db"
+        password: "root",
+        database: "burgers_db",
+        port: 8889
     });
-}
+
 
 connection.connect();
 
