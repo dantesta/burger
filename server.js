72eaa221a7c7f544fa69280aefe9103feff0e15d
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-11-03 17:19:42 -0400

    models finished

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
+app.use(express.static(process.cwd() + "/public"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var burgersController = require("./controllers/burgers_controller.js");
+
+app.use("/", burgersController);
 
 app.listen(PORT);
